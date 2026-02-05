52d11a2d54cabdaf430ae6e337f51d6b39d80242
njnkna-arch <njnkna@gmail.com>
2026-02-05 16:32:09 +0900

    Update DBConnection.java

--- src/main/java/com/lumina/util/DBConnection.java
+++ src/main/java/com/lumina/util/DBConnection.java
@@ -1,11 +1,18 @@
 package com.lumina.util;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/**
+ * データベース接続を管理するクラス。
+ * MySQL 9.x (Railway) の最新セキュリティ設定に対応した最終版です。
+ */
 public class DBConnection {
     public static Connection getConnection() throws Exception {
+        // JDBCドライバのロード
         Class.forName("com.mysql.cj.jdbc.Driver");
         
+        // Railwayの環境変数を取得
         String host = System.getenv("MYSQLHOST");
         String port = System.getenv("MYSQLPORT");
         String dbName = System.getenv("MYSQLDATABASE");
@@ -14,12 +21,22 @@ public class DBConnection {
 
         String url;
         if (host != null) {
-            // 末尾にセキュリティ許可設定を追加
-            url = String.format("jdbc:mysql://%s:%s/%s?serverTimezone=JST&useUnicode=yes&characterEncoding=UTF-8&allowPublicKeyRetrieval=true&useSSL=false", host, port, dbName);
+            // 【重要】最新のMySQLに対応するため、末尾にセキュリティ許可設定をすべて追加しました
+            url = String.format(
+                "jdbc:mysql://%s:%s/%s?serverTimezone=JST" +
+                "&useUnicode=true&characterEncoding=UTF-8" +
+                "&allowPublicKeyRetrieval=true" +
+                "&useSSL=false" +
+                "&connectionAttributes=program_name:LuminaApp", 
+                host, port, dbName
+            );
         } else {
-            url = "jdbc:mysql://localhost:3306/luminadb?serverTimezone=JST&useUnicode=yes&characterEncoding=UTF-8";
-            user = "root"; pass = "root";
+            // ローカル（Eclipse）用
+            url = "jdbc:mysql://localhost:3306/luminadb?serverTimezone=JST&allowPublicKeyRetrieval=true&useSSL=false";
+            user = "root";
+            pass = "root"; 
         }
+
         return DriverManager.getConnection(url, user, pass);
     }
 }
